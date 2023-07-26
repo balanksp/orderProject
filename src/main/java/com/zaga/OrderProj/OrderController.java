c73041eff936151a576fb6c47ab8871247478858
Anushiya <anushiyadhamu@gmail.com>
2023-07-26 15:55:04 +0530

    added loggeer

--- src/main/java/com/zaga/OrderProj/OrderController.java
+++ src/main/java/com/zaga/OrderProj/OrderController.java
@@ -1,5 +1,7 @@
 package com.zaga.OrderProj;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,20 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+        private static final Logger logger = LogManager.getLogger(OrderController.class);
 
         @Autowired
         OrderService orderService;
 
         @PostMapping("/createOrders")
         public OrderEntity createOrders(@RequestBody OrderEntity orderEntity) {
+                logger.info("Received request to create order: {}", orderEntity);
                 OrderEntity details = orderService.createOrderDetails(orderEntity);
+                logger.info("Order created successfully: {}", details);
                 return details;
         }
 
         @GetMapping("/getOrders")
         public ResponseEntity<OrderEntity> getOrdersByid(Long id) {
+                logger.info("Received request to get order by ID: {}", id);
                 OrderEntity getDetails = orderService.getOrderDetails(id);
-                return ResponseEntity.ok(getDetails);
+                if (getDetails != null) {
+                        logger.info("Found order with ID {}: {}", id, getDetails);
+                        return ResponseEntity.ok(getDetails);
+                } else {
+                        logger.warn("No order found with ID: {}", id);
+                        return ResponseEntity.notFound().build();
+                }
         }
 
 }
